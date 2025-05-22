36ac80e11d20dc86190e99552debe6dd758e881b
Ashiqur <ashiqur2812@gmail.com>
2025-05-23 00:23:41 +0600

    practised extract and some other functionality

--- groupBy.sql
+++ groupBy.sql
@@ -5,90 +5,74 @@ WHERE
 GROUP BY
     department;
 
+SELECT status, COUNT(*)
+FROM students_data
+WHERE
+    status = 'A+'
+GROUP BY
+    status;
 
-SELECT status, COUNT(*) FROM students_data
-WHERE status = 'A+'
-GROUP BY status;
+SELECT department FROM students_data GROUP BY department;
 
-SELECT department FROM students_data
-GROUP BY department;
+SELECT status FROM students_data GROUP BY status;
 
-SELECT status FROM students_data
-GROUP BY status;
+SELECT name, status FROM students_data GROUP BY name, status;
 
-SELECT name, status FROM students_data
-GROUP BY name, status;
+SELECT department, COUNT(*), AVG(age), SUM(score)
+FROM students_data
+GROUP BY
+    department
+HAVING
+    avg(age) > 22;
 
-SELECT department, COUNT(*), AVG(age), SUM(score) FROM students_data
-GROUP BY department
-HAVING avg(age) > 22;
-
-SELECT department, COUNT(*), AVG(age) FROM students_data
-GROUP BY department
-HAVING AVG(age) > 22;
+SELECT department, COUNT(*), AVG(age)
+FROM students_data
+GROUP BY
+    department
+HAVING
+    AVG(age) > 22;
 
 ALTER TABLE students_data ADD COLUMN dob DATE;
 
-UPDATE students_data
-SET dob = '2001-06-12'
-WHERE id = 1;
-UPDATE students_data
-SET dob = '2001-02-22'
-WHERE id = 2;
-UPDATE students_data
-SET dob = '2000-11-11'
-WHERE id = 3;
-UPDATE students_data
-SET dob = '2002-03-20'
-WHERE id = 4;
-UPDATE students_data
-SET dob = '2001-12-19'
-WHERE id = 5;
-UPDATE students_data
-SET dob = '2004-03-21'
-WHERE id = 6;
-UPDATE students_data
-SET dob = '2003-01-02'
-WHERE id = 7;
-UPDATE students_data
-SET dob = '2005-01-30'
-WHERE id = 8;
-UPDATE students_data
-SET dob = '2001-07-26'
-WHERE id = 9;
-UPDATE students_data
-SET dob = '2003-11-08'
-WHERE id = 10;
-UPDATE students_data
-SET dob = '2000-09-29'
-WHERE id = 11;
-UPDATE students_data
-SET dob = '2000-11-18'
-WHERE id = 12;
-UPDATE students_data
-SET dob = '2000-06-18'
-WHERE id = 13;
-UPDATE students_data
-SET dob = '2001-02-05'
-WHERE id = 14;
-UPDATE students_data
-SET dob = '2003-06-12'
-WHERE id = 15;
-UPDATE students_data
-SET dob = '2005-06-29'
-WHERE id = 16;
-UPDATE students_data
-SET dob = '2001-12-25'
-WHERE id = 17;
-UPDATE students_data
-SET dob = '2004-09-18'
-WHERE id = 18;
-UPDATE students_data
-SET dob = '2001-08-15'
-WHERE id = 19;
-UPDATE students_data
-SET dob = '2001-12-24'
-WHERE id = 20;
+UPDATE students_data SET dob = '2001-06-12' WHERE id = 1;
+
+UPDATE students_data SET dob = '2001-02-22' WHERE id = 2;
+
+UPDATE students_data SET dob = '2000-11-11' WHERE id = 3;
+
+UPDATE students_data SET dob = '2002-03-20' WHERE id = 4;
+
+UPDATE students_data SET dob = '2001-12-19' WHERE id = 5;
+
+UPDATE students_data SET dob = '2004-03-21' WHERE id = 6;
+
+UPDATE students_data SET dob = '2003-01-02' WHERE id = 7;
+
+UPDATE students_data SET dob = '2005-01-30' WHERE id = 8;
+
+UPDATE students_data SET dob = '2001-07-26' WHERE id = 9;
+
+UPDATE students_data SET dob = '2003-11-08' WHERE id = 10;
+
+UPDATE students_data SET dob = '2000-09-29' WHERE id = 11;
+
+UPDATE students_data SET dob = '2000-11-18' WHERE id = 12;
+
+UPDATE students_data SET dob = '2000-06-18' WHERE id = 13;
+
+UPDATE students_data SET dob = '2001-02-05' WHERE id = 14;
+
+UPDATE students_data SET dob = '2003-06-12' WHERE id = 15;
+
+UPDATE students_data SET dob = '2005-06-29' WHERE id = 16;
+
+UPDATE students_data SET dob = '2001-12-25' WHERE id = 17;
+
+UPDATE students_data SET dob = '2004-09-18' WHERE id = 18;
+
+UPDATE students_data SET dob = '2001-08-15' WHERE id = 19;
+
+UPDATE students_data SET dob = '2001-12-24' WHERE id = 20;
 
 SELECT EXTRACT(YEAR FROM dob) as birth_year, COUNT(*) FROM students_data
 GROUP BY birth_year;
--- practice_01.sql
+++ practice_01.sql
@@ -129,5 +129,102 @@ GROUP BY
 ORDER BY avg_salary DESC
 LIMIT 1;
 
-SELECT EXTRACT(YEAR FROM hire_date) AS hire_year, COUNT(*) FROM employees
-GROUP BY hire_year;
+SELECT EXTRACT(
+        YEAR
+        FROM hire_date
+    ) AS hire_year, COUNT(*)
+FROM employees
+GROUP BY
+    hire_year;
+
+SELECT EXTRACT(
+        MONTH
+        FROM hire_date
+    ) AS hire_month, COUNT(*)
+FROM employees
+GROUP BY
+    hire_month;
+
+SELECT department_name, ROUND(AVG(salary), 2) as avg_salary
+FROM employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name
+ORDER BY avg_salary DESC
+LIMIT 1;
+
+SELECT EXTRACT(
+        YEAR
+        FROM hire_date
+    ) AS hire_year, COUNT(*)
+FROM employees
+GROUP BY
+    hire_year;
+
+SELECT hire_date::DATE AS hire_day, COUNT(*)
+FROM employees
+GROUP BY
+    hire_day;
+
+SELECT COUNT(*), EXTRACT(
+        YEAR
+        FROM hire_date
+    ) AS hired_standard
+FROM employees
+GROUP BY
+    hired_standard;
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO
+    orders (
+        customer_id,
+        order_date,
+        total_amount
+    )
+VALUES (1, '2005-01-23', 100.05),
+    (2, '2005-02-23', 100.10),
+    (3, '2005-03-23', 100.20),
+    (4, '2005-04-21', 100.30),
+    (1, '2005-04-20', 100.40),
+    (2, '2006-04-27', 100.50),
+    (3, '2007-04-23', 100.60),
+    (4, '2022-04-23', 100.70),
+    (1, '2022-04-17', 100.80),
+    (2, '2022-02-10', 100.90);
+
+SELECT * FROM orders;
+
+DROP TABLE orders;
+
+-- Find customers who have placed more than two orders and calculate the total amount spent by each of these customers
+
+SELECT
+    customer_id,
+    COUNT(order_id) AS place_order,
+    SUM(total_amount) AS amount_of_money
+FROM orders
+GROUP BY
+    customer_id
+HAVING
+    COUNT(order_id) > 2;
+
+-- Find the total amount of orders placed each month in the year 2022
+SELECT * FROM orders;
+
+SELECT
+    EXTRACT(MONTH FROM order_date) AS getMonth,
+    EXTRACT(YEAR FROM order_date) AS getYear,
+    SUM(total_amount) AS amount_of_money
+FROM orders
+WHERE EXTRACT(YEAR FROM order_date) = 2005
+GROUP BY getMonth, getYear
+ORDER BY getMonth;
+
+
+
