8ae231eb5c227ba1d27331eeedc0cfa632e1aefb
Ashiqur <ashiqur2812@gmail.com>
2025-11-04 09:21:08 +0600

    extra practise

--- prac_alter.sql
+++ prac_alter.sql
@@ -20,3 +20,20 @@ ALTER TABLE employee alter COLUMN user_name type VARCHAR(60)
 ALTER TABLE employee alter COLUMN user_email set NOT NULL;
 
 ALTER TABLE employee alter COLUMN user_email DROP NOT NULL;
+
+INSERT INTO employee (user_name, age) VALUES ('sagor', 22);
+
+ALTER TABLE employee
+alter COLUMN user_email
+set DEFAULT 'test@gmail.com';
+
+ALTER TABLE employee
+ADD constraint unique_employee_email UNIQUE (user_email);
+
+ALTER TABLE employee alter COLUMN user_email DROP DEFAULT
+
+ALTER TABLE employee 
+ADD constraint pk_employee_id PRIMARY KEY(id)
+
+ALTER TABLE employee
+DROP constraint pk_employee_id;
